ee5b65e4925ca8da74d7ef419784c410cc9811de
rakesh2022 <83332290+rakesh2022@users.noreply.github.com>
2022-04-07 23:46:01 +0530

    Time: 5 ms (23.95%), Space: 8.7 MB (92.06%) - LeetHub

--- 27-remove-element/27-remove-element.cpp
+++ 27-remove-element/27-remove-element.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int i=0,j=nums.size()-1;
-        while(i<j){
-            while(i<j && nums[i]!=val)i++;
-            while(i<j && nums[j]==val)j--;
-            swap(nums[i],nums[j]);
-            i++;j--;
-        }
-        i=0;
-        int k=0;
-        while(i<nums.size()){
-            if(nums[i]==val)return k;
-            i++;
-            k++;
-        }
-        return k;
+        int slow=0,fast=0;
+       while(fast<nums.size()){
+           if(nums[fast]!=val){
+               nums[slow]=nums[fast];
+               slow++;
+           }
+           fast++;
+       }
+        return slow;
     }
 };
