478405a288d02a0188491851769d310396acf511
rakesh2022 <83332290+rakesh2022@users.noreply.github.com>
2022-05-20 12:41:16 +0530

    Time: 23 ms (77.36%), Space: 23.1 MB (93.82%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -5,9 +5,8 @@ public:
         if(n<2)return;
         int i=0,j=n-1;
         while(i<j){
-            char temp=s[i];
-            s[i]=s[j];
-            s[j]=temp;
+            
+            swap(s[i],s[j]);
             i++;j--;
         }
     
