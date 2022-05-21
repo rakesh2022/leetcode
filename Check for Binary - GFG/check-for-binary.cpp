e71d09e567e022b6bd0ae3d63c78e3d2f74f1166
rakesh2022 <83332290+rakesh2022@users.noreply.github.com>
2022-05-21 17:56:23 +0530

    Added solution - LeetHub

--- Check for Binary - GFG/check-for-binary.cpp
+++ Check for Binary - GFG/check-for-binary.cpp
@@ -25,9 +25,8 @@ int main()
 // Return true if str is binary, else false
 bool isBinary(string str)
 {
-    for(int i=0;i<str.length();i++){
-       if(!(str[i]=='0'  || str[i]=='1'))return 0;
+    for(int i=0;i<str.length();i++)
+       if(str[i]!='0'  && str[i]!='1')return 0;
        
-   }
    return 1;
 }
