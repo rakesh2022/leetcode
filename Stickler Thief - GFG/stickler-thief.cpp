45ad7e355dcc9960e32c43b33bccd2b762c74c84
rakesh2022 <83332290+rakesh2022@users.noreply.github.com>
2022-05-23 13:01:36 +0530

    Added solution - LeetHub

---
+++ Stickler Thief - GFG/stickler-thief.cpp
@@ -0,0 +1,46 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the maximum money the thief can get.
+    int FindMaxSum(int arr[], int n)
+    {
+      int inc_cur=arr[0],exc_cur=0;
+      for(int i=1;i<n;i++){
+          int tem_exc=max(inc_cur,exc_cur);
+          int tem_inc=exc_cur+arr[i];
+          inc_cur=tem_inc;
+          exc_cur=tem_exc;
+      }
+      return max(inc_cur,exc_cur);
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    //taking total testcases
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    //taking number of houses
+		int n;
+		cin>>n;
+		int a[n];
+		
+		//inserting money of each house in the array
+		for(int i=0;i<n;++i)
+			cin>>a[i];
+		Solution ob;
+		//calling function FindMaxSum()
+		cout<<ob.FindMaxSum(a,n)<<endl;
+	}
+	return 0;
+}
+  // } Driver Code Ends
