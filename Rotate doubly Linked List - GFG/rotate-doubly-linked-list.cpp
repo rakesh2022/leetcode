4ecf919ee31b42a39c6f7404753bdd8aec80503e
rakesh2022 <83332290+rakesh2022@users.noreply.github.com>
2022-05-29 22:37:09 +0530

    Added solution - LeetHub

---
+++ Rotate doubly Linked List - GFG/rotate-doubly-linked-list.cpp
@@ -0,0 +1,111 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef struct node
+{
+  int data;
+  struct node*next,*prev;
+  
+  node(int x){
+      data = x;
+      next = NULL;
+      prev = NULL;
+  }
+  
+} Node;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+/*
+typedef struct node
+{
+  int data;
+  struct node*next,*prev;
+  
+  node(int x){
+      data = x;
+      next = NULL;
+      prev = NULL;
+  }
+  
+} Node;
+*/
+
+class Solution {
+public:
+    Node *rotateDLL(Node *start,int p)
+    {
+        Node* ptr=start;
+        while(ptr->next){
+            ptr=ptr->next;
+        }
+        ptr->next=start;
+        start->prev=ptr;
+        p--;
+        ptr=start;
+        while(p--){
+            ptr=ptr->next;
+        }
+        start=ptr->next;
+        ptr->next->prev=NULL;
+        ptr->next=NULL;
+        return start;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,p;
+        cin>>n>>p;
+        struct node*start = NULL;
+        struct node* cur = NULL;
+        struct node* ptr = NULL;
+        
+        for(int i=0;i<n;i++)
+        {
+            int a;
+            cin>>a;
+            ptr=new node(a);
+            ptr->data=a;
+            ptr->next=NULL;
+            ptr->prev=NULL;
+            if(start==NULL)
+            {
+                start=ptr;
+                cur=ptr;
+            }
+            else
+            {
+                cur->next=ptr;
+                ptr->prev=cur;
+                cur=ptr;
+            }
+        }
+        Solution obj;
+        struct node*str=obj.rotateDLL(start,p);
+        while(1)
+        {
+            cout<<str->data<<" ";
+            if(str->next==NULL)break;
+            str=str->next;
+        }
+        while(str!=NULL)
+        {
+            cout<<str->data<<" ";
+            str=str->prev;
+        }
+        cout<< "\n";   
+    }
+}
+
+  // } Driver Code Ends
