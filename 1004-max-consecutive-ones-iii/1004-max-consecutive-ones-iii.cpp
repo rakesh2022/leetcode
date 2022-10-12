af1d1cae6249baea921225bb3464c4f446d35cab
rakesh2022 <83332290+rakesh2022@users.noreply.github.com>
2022-10-12 19:28:54 +0530

    Time: 55 ms (98.82%), Space: 55.6 MB (14.22%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -9,9 +9,10 @@ public:
 //         return j-i;
               int i = 0, j = 0, cnt = 0, N = A.size(), ans = 0;
         while (j < N) {
-            cnt += A[j++] == 0;
+            cnt += A[j] == 0;
             while (cnt > k) cnt -= A[i++] == 0;
-            ans = max(ans, j - i);
+            ans = max(ans, j - i+1);
+            j++;
         }
         return ans;
         
