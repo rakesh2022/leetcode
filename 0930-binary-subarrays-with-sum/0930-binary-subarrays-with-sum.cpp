24b39fd1356cd1386a2f80ea776229cfba97e8a6
rakesh2022 <83332290+rakesh2022@users.noreply.github.com>
2022-10-12 20:17:16 +0530

    Time: 66 ms (74.78%), Space: 28.8 MB (91.37%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -4,13 +4,14 @@ public:
      int i=0,j=0,sum=0,ans=0;
         for(;j<A.size();j++){
             sum+=A[j];
-            while(sum>goal)sum-=A[i++];
+            while(i<=j && sum>goal)sum-=A[i++];
+            if(i<=j)
             ans+=(j-i+1);
         }
        return ans;
     }
     int numSubarraysWithSum(vector<int>& A, int goal) {
-        if(goal==0)return atMostGoal(A,goal);
+        // if(goal==0)return atMostGoal(A,goal);
        return atMostGoal(A,goal)-atMostGoal(A,goal-1);
     }
 };
