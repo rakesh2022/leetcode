9ef3ca568f9a1bd85a91be5c1708c0f19e28f934
rakesh2022 <83332290+rakesh2022@users.noreply.github.com>
2022-10-13 20:49:34 +0530

    Time: 2506 ms (23.88%), Space: 14.4 MB (83.47%) - LeetHub

--- 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    int ans;
-    vector<vector<int>>t;
-    int solve(vector<vector<int>>& mat, int r,int dif){
-        if(r==mat.size()){
-         return abs(dif);
-         
+   int dp[5001][71]; //5000*71
+    int row, col;
+    
+    int find(vector<vector<int>>& mat, int r, int sum, int& target)
+    {
+        if(r==row)
+            return abs(sum-target);
+        
+       if(dp[sum][r]!=-1)
+           return dp[sum][r];
+        
+        int minDiff = INT_MAX;
+        for(int j=0; j<col; j++)
+        {
+            minDiff = min(minDiff, find(mat, r+1, sum+mat[r][j], target));
         }
-        if(t[r][dif+5000]!= -1)return t[r][dif+ 5000];
-        int res=INT_MAX;
-        for(int c=0;c<mat[r].size();c++){
-           
-            res=min(res,solve(mat,r+1,dif-mat[r][c]));
-        }
-        return t[r][dif+ 5000]=res;
+        return dp[sum][r] = minDiff;
     }
-    int minimizeTheDifference(vector<vector<int>>& mat, int tar) {
-    ans=INT_MAX;
-        t.resize(71,vector<int>(10000,-1));
-        for(int i=0;i<mat.size();i++){
-            sort(mat[i].begin(), mat[i].end());
-            mat[i].erase(unique(mat[i].begin(), mat[i].end()),mat[i].end());
-        }
-       return solve(mat,0,tar);
-        // return ans;
+    int minimizeTheDifference(vector<vector<int>>& mat, int target) {
+        memset(dp,-1,sizeof(dp));
+        row = mat.size();
+        col = mat[0].size();
+        return find(mat, 0, 0, target);
     }
 };
