8b5648b1bbd881aae82ae7002b7f2400066a0b65
rakesh2022 <83332290+rakesh2022@users.noreply.github.com>
2022-10-23 12:54:05 +0530

    Time: 7 ms (9.45%), Space: 7.8 MB (68.32%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.cpp
+++ 0836-rectangle-overlap/0836-rectangle-overlap.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     bool isRectangleOverlap(vector<int>& r1, vector<int>& r2) {
-        if(!(r1[2]<=r2[0] || r2[2]<=r1[0] || r2[3]<=r1[1] || r1[3]<=r2[1]))return true;
+        // if(!(r1[2]<=r2[0] || r2[2]<=r1[0] || r2[3]<=r1[1] || r1[3]<=r2[1]))return true;
+        // return false;
+        if(r1[0]<r2[2] && r1[2]>r2[0] && r1[1]<r2[3] && r1[3]>r2[1])return true;
         return false;
     }
 };
