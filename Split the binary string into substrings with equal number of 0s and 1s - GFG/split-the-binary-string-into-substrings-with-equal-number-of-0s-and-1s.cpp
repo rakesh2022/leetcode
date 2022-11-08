a6336ac8ce0c8c3694f9313a4e89311e0b9f4461
rakesh2022 <83332290+rakesh2022@users.noreply.github.com>
2022-11-08 15:05:09 +0530

    Added solution - LeetHub

--- Split the binary string into substrings with equal number of 0s and 1s - GFG/split-the-binary-string-into-substrings-with-equal-number-of-0s-and-1s.cpp
+++ Split the binary string into substrings with equal number of 0s and 1s - GFG/split-the-binary-string-into-substrings-with-equal-number-of-0s-and-1s.cpp
@@ -8,19 +8,16 @@ using namespace std;
 class Solution {
 public:
     int maxSubStr(string str){
-        int n=str.size();
-        if(n&1)return -1;
-        if(n==0)return 0;
-        int sum=0;
-        for(int i=0;i<str.size();i++){
-            sum+= str[i]=='1'?1:-1;
-            if(sum==0){
-                int nxt= maxSubStr(str.substr(i+1));
-                if(nxt== -1)return -1;
-                return 1+ nxt;
-        }
-        }
-        return -1;
+       int i=0,j=0,sum=0,cnt=0;
+       for(;j<str.size();j++){
+           sum+= (str[j]=='1')?1:-1;
+           if(sum==0){
+               cnt++;
+           }
+        //   cout<<sum<<" "<<cnt<<endl;
+       }
+       if(sum!=0)return -1;
+       return cnt;
         
     }
 };
