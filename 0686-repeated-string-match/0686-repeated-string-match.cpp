f6053432efcef646cf374e927fd2dc46fc28802f
rakesh2022 <83332290+rakesh2022@users.noreply.github.com>
2022-11-09 18:27:51 +0530

    Time: 6 ms (89.73%), Space: 7.2 MB (52.90%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.cpp
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -1,14 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-    int repeatedStringMatch(string a, string b) {
-        
-        for(int i=0,j=0;i<a.size();i++){
-            for(j=0;j<b.size() && a[(i+j)% a.size()] == b[j]; j++);
-            if(j==b.size()){
-                return (j+i-1)/a.size() + 1;
+    vector<int> findLps(string s){
+        int n=s.length();
+        vector<int>lps(n);
+        int i=0,j=1;
+        while(j<n){
+            if(s[i]==s[j]){
+                lps[j++]=++i;
+                // i++;
+            }
+            else{
+                if(i!=0)i=lps[i-1];
+                else j++;
             }
         }
-        return -1;
+        return lps;
         
+    }
+    int repeatedStringMatch(string a, string b) {
+        
+        // for(int i=0,j=0;i<a.size();i++){
+        //     for(j=0;j<b.size() && a[(i+j)% a.size()] == b[j]; j++);
+        //     if(j==b.size()){
+        //         return (j+i-1)/a.size() + 1;
+        //     }
+        // }
+        // return -1;
+        vector<int>lps=findLps(b);
+        int i=0,j=0;
+        while(i<a.size()){
+            while(j<b.size() && a[(i+j)%a.size()]==b[j]) j++;
+            if(j==b.size())return (j+i-1)/a.size() +1;
+            if(j!=0){
+                i+= (j-lps[j-1]);
+                j=lps[j-1];
+            }
+            else
+                i++;
+        }
+        return -1;
     }
 };
