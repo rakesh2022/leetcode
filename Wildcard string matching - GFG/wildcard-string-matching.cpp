ac2e81f555bcac6010ca1d6449ba5e99a7e3fa60
rakesh2022 <83332290+rakesh2022@users.noreply.github.com>
2022-11-11 15:31:29 +0530

    Added solution - LeetHub

--- Wildcard string matching - GFG/wildcard-string-matching.cpp
+++ Wildcard string matching - GFG/wildcard-string-matching.cpp
@@ -6,29 +6,56 @@ using namespace std;
 // User function template for C++
 class Solution{
     public:
-    bool backtrack(string wld,string pat){
-        if(wld.empty() && pat.empty())return 1;
-        if(wld.empty())return 0;
-        if(pat.empty()){
-            int i=0;
-            while(i<wld.size()){
-                if(wld[i] != '*'){
+    // bool backtrack(string wld,string pat){
+    //     if(wld.empty() && pat.empty())return 1;
+    //     if(wld.empty())return 0;
+    //     if(pat.empty()){
+    //         int i=0;
+    //         while(i<wld.size()){
+    //             if(wld[i] != '*'){
+    //                 break;
+    //             }
+    //             i++;
+    //         }
+    //         return i==wld.size();
+    //     }
+    //     if(wld[0]==pat[0] || wld[0]=='?')
+    //     return backtrack(wld.substr(1),pat.substr(1));
+    //     else if(wld[0]=='*'){
+    //         return backtrack(wld.substr(1),pat.substr(1)) || backtrack(wld,pat.substr(1)) || backtrack(wld.substr(1),pat);
+    //     }
+    //     return false;
+    // }
+    bool match(string wld, string pat)
+    {
+        vector<vector<int>>t(1001,vector<int>(1001));
+        for(int i=0;i<=wld.size();i++){
+           for(int j=0;j<=pat.size();j++){
+               if(i==0 && j==0)t[i][j]=1;
+               else if(j==0){
+                   int k=0;
+            while(k<i){
+                if(wld[k] != '*'){
                     break;
                 }
-                i++;
+                k++;
             }
-            return i==wld.size();
+           t[i][j]= (k==i);
+               }
+           }
         }
-        if(wld[0]==pat[0] || wld[0]=='?')
-        return backtrack(wld.substr(1),pat.substr(1));
-        else if(wld[0]=='*'){
-            return backtrack(wld.substr(1),pat.substr(1)) || backtrack(wld,pat.substr(1)) || backtrack(wld.substr(1),pat);
+           for(int i=1;i<=wld.size();i++){
+               for(int j=1;j<=pat.size();j++){
+                   if(wld[i-1]==pat[j-1] || wld[i-1]=='?')
+                        t[i][j]= t[i-1][j-1];
+                    else if(wld[i-1]=='*'){
+                        t[i][j]= t[i-1][j-1] || t[i][j-1] || t[i-1][j];
         }
-        return false;
-    }
-    bool match(string wild, string pattern)
-    {
-       return backtrack(wild,pattern);
+               }
+           }
+        
+        // for()
+        return t[wld.size()][pat.size()];
        
     }
 };
