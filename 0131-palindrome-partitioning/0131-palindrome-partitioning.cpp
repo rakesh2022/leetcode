6d093da8cd82992163518d26fd4fe717cd626c16
rakesh2022 <83332290+rakesh2022@users.noreply.github.com>
2022-11-11 20:11:25 +0530

    Time: 328 ms (27.66%), Space: 75.7 MB (37.39%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     vector<vector<string>>res;
-    bool palind(string &s,int i,int j){
+    bool palind(string s,int i,int j){
         while(i<j){
             if(s[i++]!=s[j--])return false;
             
         }
         return true;
     }
-    void mcm(string &s,int i,int j,vector<string>&temp){
-        if(i>j){
+    void mcm(string s,vector<string>&temp){
+        if(s.empty()){
             res.push_back(temp);
             return;
         }
         
             
-            for(int k=i;k<=j;k++){
-                if(palind(s,i,k)){
-                    temp.push_back(s.substr(i,k-i+1));
+            for(int k=1;k<=s.length();k++){
+                if(palind(s,0,k-1)){
+                    temp.push_back(s.substr(0,k));
                     // if(palind(s,k+1,j)){
                     //     temp.push_back(s.substr(k+1));
                     //     mcm(s,k+1,j,temp);
                     //     temp.pop_back();
                     // }
-                    mcm(s,k+1,j,temp);
+                    mcm(s.substr(k),temp);
                     temp.pop_back();
                     }
             }
@@ -33,7 +33,7 @@ public:
         res.clear();
         vector<string>temp;
         // temp.push_back(0);
-        mcm(s,0,s.length()-1,temp);
+        mcm(s,temp);
         return res;
     }
 };
