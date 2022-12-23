7b2094a051103b4d9dde3f0f87cac5b3a427d91f
rakesh2022 <83332290+rakesh2022@users.noreply.github.com>
2022-12-23 13:26:31 +0530

    Time: 39 ms (14.99%), Space: 15.7 MB (96.85%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<int>res;
-        int n= nums.size();
-        int freq=0,val=nums[0];
+        int cnt1=0,cnt2=0,num1=0,num2=1;
+        int n=nums.size();
         for(int i=0;i<n;i++){
-            if(nums[i] == val)
-                freq++;
+            if(nums[i]==num1)
+                cnt1++;
+            else if(nums[i]==num2)
+                cnt2++;
+            else if(cnt1==0){
+                cnt1++;
+                num1= nums[i];
+            }
+            else if(cnt2==0){
+                cnt2++;
+                num2= nums[i];
+            }
             else{
-                if(freq > n/3)
-                res.push_back(val);
-                val=nums[i];
-                freq=1;
+                cnt1--;
+                cnt2--;
             }
         }
-        if(freq > n/3)
-                res.push_back(val);
+        vector<int>res;
+        cnt1=0,cnt2=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==num1)
+                cnt1++;
+            else if(nums[i]==num2)
+                cnt2++;
+        }
+        if(cnt1 > n/3)res.push_back(num1);
+        if(cnt2 > n/3)res.push_back(num2);
         return res;
     }
 };
