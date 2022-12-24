30d95e68c1dfb513f45eb192b45f778a750a5c2d
rakesh2022 <83332290+rakesh2022@users.noreply.github.com>
2022-12-24 09:36:09 +0530

    Time: 3 ms (45.28%), Space: 6 MB (82.34%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,15 +1,19 @@
 
 class Solution {
 public:
-    int t[101][101];
-    int paths(int m,int n){
-         if(m==1 || n==1)return 1;
-        if(t[m][n] != -1)return t[m][n];
-        return t[m][n]= paths(m,n-1) + paths(m-1,n);
-    }
     int uniquePaths(int m, int n) {
-        memset(t,-1,sizeof(t));
-        return paths(m,n);
+        
+    int t[m+1][n+1];
+        memset(t,0,sizeof(t));
+        for(int i=0;i<=m;i++)t[i][1]=1;
+        
+        for(int i=0;i<=n;i++)t[1][i]=1;
+        for(int i=2;i<=m;i++){
+            for(int j=2;j<=n;j++){
+                t[i][j]= t[i-1][j] + t[i][j-1];
+            }
+        }
+        return t[m][n];
       
     }
 };
